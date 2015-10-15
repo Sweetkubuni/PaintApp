493263cf24a70b91f0f56ddac4cb137ae96553c1
Sweetkubuni <justinsweng@gmail.com>
2015-10-15 16:30:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,31 +5,15 @@ var fs = require('fs');
 var server = http.createServer(function(req, res){
 	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress ||req.connection.socket.remoteAddress;
 	console.log('connection:' + ip );
-	var path = url.parse(req.url).pathname;
-	
-	switch(path)
-	{
-		case '/':
-		fs.readFile(__dirname + '/index.html',function(err,data)
-		{
-			if(err)
-			{
-				res.writeHead(404);
-				res.write("opps this doesn't exist - 404");
-			}
-			else {
-			res.writeHead(200, {"Content-Type": "text/html"});
-			res.write(data, "utf8");
-			}
-		});
-		break;
-		
-		default:
-		res.writeHead(404);
-        	res.write("opps this doesn't exist - 404");
-		break;
-	}
-	 res.end();
+	 fs.readFile(__dirname + '/index.html',function (err, data) {
+	 	if (err)
+	 	{
+      			res.writeHead(500);
+      			return res.end('Error loading index.html');
+	 	}
+	 	res.writeHead(200);
+    		res.end(data);
+    });
 });
 
 server.listen(8001);
